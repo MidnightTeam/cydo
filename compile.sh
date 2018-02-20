5bf9780673aa2afa540c33d7876f10366e38c1e3
Ben Sparkes <bensparkes8@gmail.com>
2018-02-20 03:37:59 +0000

    Removed output file, compile to cydo.out

--- compile.sh
+++ compile.sh
@@ -1,8 +1,9 @@
 currDir=$(dirname $0)
+outFile=$currDir/cydo.out
 
-rm -f $currDir/cydo
-xcrun --sdk iphoneos gcc -arch arm64 -framework Foundation $currDir/cydo.c -o $currDir/cydo
-jtool --sign sha1 --inplace --ent $currDir/ent.plist $currDir/cydo
-sudo chown root:wheel $currDir/cydo
-sudo chmod 6755 $currDir/cydo
+rm -f $outFile     
+xcrun --sdk iphoneos gcc -arch arm64 -framework Foundation $currDir/*.c -o $outFile
+jtool --sign sha1 --inplace --ent $currDir/ent.plist $outFile         
+sudo chown root:wheel $outFile     
+sudo chmod 6755 $outFile     
 echo 'Done.'
